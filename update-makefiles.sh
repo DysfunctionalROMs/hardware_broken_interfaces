476be76453eb479fe24112251b6e08ab125bcf7e
TeamFahQ <TeamFahQ>
2019-05-20 21:53:13 -0500

    Broken rebase 9.0

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.slim:hardware/slim/interfaces" \
+  "vendor.broken:hardware/broken/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
